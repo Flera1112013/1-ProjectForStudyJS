412e45361e8ea64bf72a8682aa170b5950d58419
kirillbatutin <alien24679@yandex.ru>
2022-12-03 08:15:23 +0400

    first step in js

---
+++ unit/unit1/script.js
@@ -0,0 +1,21 @@
+// const quest= confirm("Сколько фильмов вы уже посмотрели?"); - просто вопрос
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+
+const personalMovieDB={
+ count: numberOfFilms,
+ movies:{},
+ actors:{},
+ genres:[],
+ privat:false
+};
+
+const questAboutTheLastFilm1 = prompt("Один из последних просмотренных фильмов?");
+const questAboutRateFilm1 = +prompt("на сколько оцените его?");
+const questAboutTheLastFilm2 = prompt("Один из последних просмотренных фильмов?");
+const questAboutRateFilm2 = +prompt("на сколько оцените его?");
+
+personalMovieDB.movies [questAboutTheLastFilm1]= questAboutRateFilm1;
+personalMovieDB.movies [questAboutTheLastFilm2]= questAboutRateFilm2;
+
+console.log(personalMovieDB);
